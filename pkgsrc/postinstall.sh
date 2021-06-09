812e139fa37774027635e6e6f22b50d20623de5e
Erik Hollensbe <linux@hollensbe.org>
2021-06-09 09:44:30 -0700

    Remove service override
    closes #9
    
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- pkgsrc/postinstall.sh
+++ pkgsrc/postinstall.sh
@@ -2,7 +2,6 @@
 
 set -xeu
 
-test -f /var/run/zerotier-one.service && mv /var/run/zerotier-one.service /lib/systemd/system
 systemctl daemon-reload
 systemctl enable zerotier-systemd-manager.timer
 systemctl start zerotier-systemd-manager.timer
